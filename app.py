34687aa02c71cc42244c36c97f8a981ceb8297d0
mohdtalha001 <syedmohdtalha0@gmail.com>
2026-08-29 12:07:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def create():
         db.session.add(new_campaign)
         db.session.commit()
         return redirect(url_for('index'))
-          return render_template('create_campaign.html')
+        return render_template('create_campaign.html')
     @app.route('/donate/<int:id>', methods=['POST'])
     def donate(id):
         campaign = Campaign.query.get_or_404(id)
